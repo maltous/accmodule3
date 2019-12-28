702a382ecf063d3dd3590d0ef7b2107269fce0e6
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2019-12-28 18:34:59 +0300

    accmodule3

---
+++ story_content/user.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6aQn2knZ7nw":
+        Script1();
+        break;
+      case "5zlhKD9A0i8":
+        Script2();
+        break;
+      case "67WehluRrP6":
+        Script3();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  window.print();
+}
+
